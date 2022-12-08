09ef29ccbc791a356716d9befa3cbc12b446d7f2
pixas <jiangshuyang@sjtu.edu.cn>
2022-12-08 11:23:22 +0800

    d

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ import torchvision.models as models
 import pprint 
 resnet18 = models.resnet18(pretrained=True)
 import torch 
-torch.save(resnet18, "resnet18.t7")
+torch.save(resnet18.state_dict(), "resnet18.t7")
